38d1e9fe80840c92315a5d3ddab966471b6a0c7f
JaniLeskela <74043777+JaniLeskela@users.noreply.github.com>
2023-12-08 18:35:40 +0200

    Add sidebar.js

---
+++ sidebar.js
@@ -0,0 +1,31 @@
+import "./sidebar.css"
+
+import Alkoholi from "./alkoholi/alkoholi"
+import Kategoria from "./kategoria/kategoria"
+import Hinta from "./hinta/hinta"
+
+
+ function Sidebar({handleChange}) {
+
+
+    return (
+   
+        <>
+        <section className="sidebar">
+          <div className="logo-container">
+
+          <h1>KG Store</h1>
+
+        </div>
+
+          <Kategoria handleChange={() => handleChange} />
+          <Alkoholi handleChange={handleChange} />
+          <Hinta handleChange={handleChange} />
+
+        </section>
+
+        
+        </>
+   
+  )
+} export default  Sidebar
